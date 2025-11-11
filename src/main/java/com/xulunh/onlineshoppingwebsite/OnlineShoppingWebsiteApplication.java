ce85535ad5e19901234f00ebdd9fe20989c452c7
Xulun Huang <xulunh01@gmail.com>
2025-11-10 21:02:47 -0500

    init

---
+++ src/main/java/com/xulunh/onlineshoppingwebsite/OnlineShoppingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.xulunh.onlineshoppingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xulunh/onlineshoppingwebsite/OnlineShoppingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xulunh.onlineshoppingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
